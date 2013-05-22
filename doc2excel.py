0e85e9f054648aa2cc4c5366ad46913af09591dd
willcai1984 <willcai1984@gmail.com>
2013-05-22 10:28:54 +0800

    Update doc2excel.py
    update the command format and example command

--- doc2excel.py
+++ doc2excel.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # Filename: doc2excel.py
-# Function: Transfer doc to excel list
+# Function: Transfer doc to excel list, only support doc not docx
 # -*- coding: UTF-8 -*-
-# Author: Well
-
+# Author: Will
+# Command Format:python doc2excel.py -df doc1_path -sn execl_sheet1 -df doc2_path -sn execl_sheet2 -ef export_excel -b 'begin_reg' -e 'end_reg'
+# Example command: python /home/python/doc2excel/linux/doc2excel.py -df 'Console_serial_port_test_case.doc' -sn 'console_serial' -ef 'xfzhou.xls' -b "^Case ID" -e "^Pre-condition"
 import os, re, sys, xlrd, xlwt, argparse
 
 def debug(mesage, is_debug=True):
